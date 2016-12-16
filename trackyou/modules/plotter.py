915730e5216ca815de2f653c0dff8238d45575a0
himkt <himkt@klis.tsukuba.ac.jp>
2016-12-17 00:35:30 +0900

    Fixbug: trackyou_data processing

--- trackyou/modules/plotter.py
+++ trackyou/modules/plotter.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 import csv
+
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
+
 import webbrowser as web
+from matplotlib import style
 
 from bottle import route
 from bottle import run
 from bottle import static_file
 
 trackyou_data = {}
+
+
 class Plotter(object):
     """ Visualization of progress class. """
 
@@ -19,29 +24,32 @@ class Plotter(object):
     def set_data(self):
         with open(self.target_path) as f:
             reader = csv.reader(f, delimiter="\t")
+
             for row in reader:
-                k = row[0].split("-")
-                if k[2] in trackyou_data:
-                    trackyou_data[k[2]] = int(trackyou_data[k[2]]) + int(row[1])
-                else:
-                    trackyou_data[k[2]] = int(row[1])
+                timestamp, num_words = row
+                trackyou_data[timestamp] = int(num_words)
 
     @route("/plot")
     def plot():
         x = []
         y = []
-        for k in sorted(trackyou_data.keys()):
-            y.append(trackyou_data[k])
-            x.append(int(k))
 
+        for i, timestamp in enumerate(sorted(trackyou_data.keys())):
+            y.append(trackyou_data[timestamp])
+            x.append(timestamp)
+
+        style.use('ggplot')
         fig = Figure()
         canvas = FigureCanvas(fig)
         ax = fig.add_subplot(211)
-        ax.plot(x, y)
-        ax.fill_between(x,y,color="skyblue",alpha=0.3)
+        ax.plot(y)
+
+        # ax.fill_between(x, y, color="skyblue", alpha=0.3) TODO
+        # ax.set_xticklabels(x) # TODO: FigureCanvas show fixed labels
+        # ax.set_xlabel('date')
+        ax.set_xticklabels([])
         ax.set_title('How many words did you write today ?')
         ax.grid(True)
-        ax.set_xlabel('date')
         ax.set_ylabel('count')
         canvas.print_figure("tmp.png")
         return static_file("tmp.png", root='./', mimetype='image/png')
